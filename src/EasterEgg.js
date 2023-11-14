c66640a43fd26cbc2c317a44bf9e11a8e5648de3
Isaac Aftergut <ijagut@vt.edu>
2023-11-14 18:56:59 -0500

    updated ee

--- src/EasterEgg.js
+++ src/EasterEgg.js
@@ -3,25 +3,27 @@ const EasterEgg = ()=> {
   return (
     <div className='easterEgg'>
       <h2>Easter Egg</h2>
-      <h3>Insightful Books I Have Read:</h3>
-      <ul>
-      <li>"She Comes First" by: Ian Kerner</li>
-      <li>"Einstein's Dreams" by: Alan Lightman</li>
-      <li>"Letters to a Young Poet" by: Rainer Maria Rilke</li>
-      <li>"Circe" by: Madeline Miller</li>
-      <li>"The Song of Achilles" by: Madeline Miller</li>
-      <li>"Come As You Are" by: Emily Nagoski</li>
-      </ul>
-      <hr/>
-      <h3>Quotes from Friends:</h3>
+      <h3>Quotes from Friends About Me:</h3>
       <ul>
       <li>"Caring, considerate, goofy, reliable, loyal, creative, precise, collector" -Best Female Friend</li>
       </ul>
     <hr/>
       <h3>Quotes I Enjoy:</h3>
       <ul>
+      <li>"...sit with a pretty girl for an hour and it feels like a minute; sit on a hot stove for a minute and it feels like many hours. That's relativity"-Albert Einstein</li>
       <li>"I don't stop eatin til the jobs done" -Joey Chestnut</li>
       </ul>
+    <hr/>
+      <h3>Insightful Books I Have Read:</h3>
+      <ul>
+      <li>"She Comes First" by: Ian Kerner</li>
+      <li>"Einstein's Dreams" by: Alan Lightman</li>
+      <li>"Circe" by: Madeline Miller</li>
+      <li>"The Song of Achilles" by: Madeline Miller</li>
+      <li>"Come As You Are" by: Emily Nagoski</li>
+      <li>"Letters to a Young Poet" by: Rainer Maria Rilke</li>
+      </ul>
+
     </div>
   );
 };
