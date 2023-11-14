3f297314169c60d210ed94e76fed025839fce092
Isaac Aftergut <ijagut@vt.edu>
2023-11-14 15:02:07 -0500

    seperating pages

---
+++ src/Links.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const Links = ()=> {
+    const renderLinkHarmonicHarbor = () => {
+        window.location.href = "https://harmonic-harbor.onrender.com";
+      };
+      const renderLinkThreeWishes= () => {
+        window.location.href = "https://three-wishes.onrender.com";
+      };
+  return (
+    <div>
+      <h2>Links</h2>
+      <h4>Visit Harmonic Harbor:</h4>
+      <button className="button" onClick={renderLinkHarmonicHarbor}>Harmonic Harbor</button>
+      <h4>Visit Three Wishes:</h4>
+      <button className="button" onClick={renderLinkThreeWishes}>Three Wishes</button>
+    </div>
+  );
+};
+
+export default Links;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+   <HashRouter><App /></HashRouter>
 );
